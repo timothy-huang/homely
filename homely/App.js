7f96123ac74c9cabff1beb608801b93fbf557b91
Timothy <thuang87@berkeley.edu>
2018-11-03 05:54:17 -0700

    created project and stuff

--- homely/App.js
+++ homely/App.js
@@ -1,62 +1,98 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import React, { Component } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity 
+} from 'react-native'
 
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
-
+export default class App extends Component {
   render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+    return (
+      <View style={styles.container}>
+        <View style={styles.container2}>
+          <View style={styles.circleContainer}>
+            <View style={styles.circle}><Text>TH</Text></View>
+            <View style={styles.circle}><Text>MK</Text></View>
+            <View style={styles.circle}><Text>ML</Text></View>
+            <View style={styles.circle}><Text>WJ</Text></View>
+          </View>
         </View>
-      );
-    }
+        <View style={styles.smallContainer}>
+          <Text style={styles.goldText}>Your assignment is</Text>
+          <Text style={styles.largeText}>trash</Text>
+          <View style={styles.underline}></View>
+        </View>
+        <View style = {styles.smallContainer}>
+          <Text style={styles.goldText}>You have</Text>
+          <Text style={styles.largeText}>3</Text>
+          <Text style={styles.goldText}>days left to complete it</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.onPress}
+        >
+        <Text style={styles.blueText}>Done</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
-
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#01539B',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  smallContainer: {
+    marginBottom: 32,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    width: 100,
+    height: 64,
+    justifyContent: 'center'
+  },
+  circleContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  button: {
+    width: 269,
+    height: 64,
+    marginTop: 32,
+    borderRadius: 100,
+    backgroundColor: '#F0C808',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  circle: {
+    width: 64,
+    height: 64,
+    borderRadius: 100,
+    backgroundColor: '#7DCE82',
+    marginRight: 8
+  },
+  goldText: {
+    fontSize: 24,
+    color: '#F0C808'
+  },
+  blueText: {
+    fontSize: 24,
+    color: '#01539B'
+  },
+  largeText: {
+    fontSize: 48,
+    color: '#FFFFFF'
+  },
+  underline: {
+    width: 150,
+    borderStyle: 'solid',
+    borderRadius: 1,
+    borderWidth: 1,
+    borderColor: '#F0C808'
+  }
 });
