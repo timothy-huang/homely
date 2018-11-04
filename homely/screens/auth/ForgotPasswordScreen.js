4a1c20cd8f87acc298022930d7ebfab94ac35e5d
mk1123 <khattar.manan@gmail.com>
2018-11-03 18:38:31 -0700

    forgot password stuff

--- homely/screens/auth/ForgotPasswordScreen.js
+++ homely/screens/auth/ForgotPasswordScreen.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import { EDEADLK } from 'constants';
 
 export default class ForgotPasswordScreen extends React.Component {
-  
     constructor(props) {
-        
+        super(props);
+        this.state = { };
+    }
+
+    render() {
+        return <Text style={{paddingTop:20}}>LoginScren</Text>
     }
 }
+
+const styles = StyleSheet.create({
+
+});
